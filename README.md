f0d3c49fafb2f75bbe99628cccb6beda7ca563f0
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2019-03-13 21:31:24 -0400

    Updated React UI

--- README.md
+++
@@ -1,1 +0,0 @@
-Simple BBB Auto School
---
+++ precache-manifest.3aa3605cc83693691e57f67967043649.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa1e81c2df4c279391494c0959ba2cd4",
+    "url": "/static/media/top_down2.fa1e81c2.jpg"
+  },
+  {
+    "revision": "8d0ddf68d69ce5c0d18fe689fe3251c9",
+    "url": "/static/media/in_highway.8d0ddf68.jpg"
+  },
+  {
+    "revision": "8be9965c1402af3f7f279b48033e2791",
+    "url": "/static/media/girl_on_road.8be9965c.jpg"
+  },
+  {
+    "revision": "e1b622f4809ccd319ffa64feeca0bbc8",
+    "url": "/static/media/behind_mirror.e1b622f4.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "958fe4cc03aaf7ffb737",
+    "url": "/static/js/main.813d9242.chunk.js"
+  },
+  {
+    "revision": "309f6894f3b4a88a44cf",
+    "url": "/static/js/2.7cacdb40.chunk.js"
+  },
+  {
+    "revision": "958fe4cc03aaf7ffb737",
+    "url": "/static/css/main.0eeb9ba8.chunk.css"
+  },
+  {
+    "revision": "309f6894f3b4a88a44cf",
+    "url": "/static/css/2.22c2ca31.chunk.css"
+  },
+  {
+    "revision": "d2b9394d41192f3c05d070ba01ae2086",
+    "url": "/index.html"
+  }
+];
